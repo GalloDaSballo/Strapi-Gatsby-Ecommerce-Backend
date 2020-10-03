de374f14c584b7ed93c2cfa17d57ac6332611773
Entreprenerd <valeranijr@gmail.com>
2020-10-03 21:48:50 +0200

    feat added pg-connection-string

--- config/database.js
+++ config/database.js
@@ -1,3 +1,6 @@
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL)
+
 module.exports = ({ env }) => {
   if(env('NODE_ENV') === 'production'){
     return {
@@ -7,11 +10,11 @@ module.exports = ({ env }) => {
           connector: 'bookshelf',
           settings: {
             client: 'postgres',
-            host: env('DATABASE_HOST', '127.0.0.1'),
-            port: env.int('DATABASE_PORT', 27017),
-            database: env('DATABASE_NAME', 'strapi'),
-            username: env('DATABASE_USERNAME', ''),
-            password: env('DATABASE_PASSWORD', ''),
+            host: config.host,
+            port: config.port,
+            database: config.database,
+            username: config.user,
+            password: config.password,
           },
           options: {
             ssl: false,
